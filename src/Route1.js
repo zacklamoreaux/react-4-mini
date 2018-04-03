d1a8cce88cfe75cd899d70dd95634d6ebf790120
Zack <zacklam42@gmail.com>
2018-04-03 15:11:51 -0600

    done

---
+++ src/Route1.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default function Route1() {
+  return (
+    <div>
+      Route 1 here!
+    </div>
+  )
+}
---
+++ src/Route3.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default function Route3() {
+  return (
+    <div>
+      Route 3 here!
+    </div>
+  )
+}
